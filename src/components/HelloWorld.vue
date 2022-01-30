b6370110d787e2fbe3b13cbbcd9da6399cffc161
Teo <teo.berguerre@axopen.com>
2022-01-30 04:15:20 +0100

    adding i18n translation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ props.msg }}</h1>
+    <h2>{{ $t('message')}}</h2>
   </div>
 </template>
 
 <script setup>
-const msg = "yikes"
+// eslint-disable-next-line no-undef
+const props = defineProps({
+  msg: String
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
