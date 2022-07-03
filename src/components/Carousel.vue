9ec53b1d14c6c711eea7116db986d7c9d53dc7fa
Teo Berguerre <teothou@hotmail.fr>
2022-07-03 16:44:58 +0200

    carousel dynamic

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,46 +1,80 @@
 <template>
   <div>
-    <div id="carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
-      <div class="carousel-inner">
-        <div  v-for="(icons, index) in listIconFight" :key="index"
-              class="carousel-item" :class="index === 0 ? 'active' : ''">
-          <img  :src="icons" class="w-25">
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
+    <c2 :list-tec="listTec" :key="refresh" />
     <div class="row" style="margin-top: 50px;">
 <!--border on hover + change color if active      -->
-      <div class="col-lg-6 p-lg-5 border">Front-End</div>
-      <div class="col-lg-6 p-lg-5 border">Back-End</div>
-      <div class="col-lg-6 p-lg-5 border">Soft-Skills</div>
-      <div class="col-lg-6 p-lg-5 border"> jsp</div>
+      <div class="col-lg-6 p-lg-4 border" :class="isFEOn ? 'active' :'' " @click="changeTec('FE')">Front-End</div>
+      <div class="col-lg-6 p-lg-4 border" :class="isBEOn ? 'active' :'' " @click="changeTec('BE')">Back-End</div>
+      <div class="col-lg-6 p-lg-4 border" :class="isSFOn ? 'active' :'' " @click="changeTec('SF')">{{$t('CAROUSEL.SKILLS.SOFTSKILL')}}</div>
+      <div class="col-lg-6 p-lg-4 border" :class="isDBOn ? 'active' :'' " @click="changeTec('DB')">{{$t('CAROUSEL.SKILLS.DATABASE')}}</div>
     </div>
   </div>
 </template>
 
 <script setup>
-const listIconFight = [
+import C2 from "@/components/c2";
+import {ref} from "vue";
+
+let isFEOn = true;
+let isBEOn,isSFOn,isDBOn = false;
+
+let listTec = ref([
   require('../assets/img/Presentation/icons/angular.svg'),
   require('../assets/img/Presentation/icons/vuejs.svg'),
   require('../assets/img/Presentation/icons/react.svg'),
-  require('../assets/img/Presentation/icons/netcore.svg')
-]
+]);
+
+function changeTec(tec) {
+  switch (tec) {
+    case 'FE':
+      listTec.value = [
+        require('../assets/img/Presentation/icons/angular.svg'),
+        require('../assets/img/Presentation/icons/vuejs.svg'),
+        require('../assets/img/Presentation/icons/react.svg'),
+      ]
+      this.isFEOn = !this.isFEOn
+      this.isDBOn = false;
+      this.isBEOn = false;
+      this.isSFOn = false;
+      break;
+
+    case 'BE':
+      listTec.value = [
+        require('../assets/img/Presentation/icons/netcore.svg')
+      ];
+      this.isBEOn = !this.isBEOn
+      this.isFEOn = false;
+      this.isDBOn = false;
+      this.isSFOn = false;
+      break;
+
+    case 'SF':
+      listTec.value = [
+        require('../assets/img/Presentation/carousel/softskills/curieux.png'),
+        require('../assets/img/Presentation/carousel/softskills/attentif.png')
+      ]
+      this.isSFOn = !this.isSFOn
+      this.isFEOn = false
+      this.isDBOn = false;
+      this.isBEOn = false;
+      break;
+
+    case 'DB':
+      listTec.value = [
+        require('../assets/img/Presentation/icons/mariadb.svg'),
+        require('../assets/img/Presentation/icons/mysql.svg')
+      ]
+      this.isDBOn = !this.isDBOn
+      this.isFEOn = false;
+      this.isBEOn = false;
+      this.isSFOn = false;
+      break;
+  }
+}
 </script>
 
 <style scoped lang="scss">
-.carousel-item {
-
-
-  img {
-    height: 200px;
-  }
+.active {
+  background-color: #8360c3;
 }
 </style>
---
+++ src/components/c2.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
+    <div class="carousel-inner">
+      <div  v-for="(icons, index) in props.listTec" :key="icons"
+            class="carousel-item" :class="index === 0 ? 'active' : ''">
+        <img  :src="icons" class="w-25" :alt="icons">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import {defineProps} from "vue";
+
+const props = defineProps({
+  listTec: Array,
+})
+
+</script>
+
+<style lang="scss" scoped>
+.carousel-item {
+
+
+  img {
+    height: 200px;
+  }
+}
+</style>
