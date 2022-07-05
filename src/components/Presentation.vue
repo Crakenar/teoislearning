f1dd6ac1cd19a022fbbb1f238ff5784376c4227c
Teo Berguerre <teothou@hotmail.fr>
2022-07-05 20:59:00 +0200

    level image + animation presentation

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -6,13 +6,18 @@
     <div class="col ">
       <!--Faire une box de code avec les couleurs si possibles pour dire mon nom prenom age addresse etc      -->
       <div class="code-block">
-        <h2 class="l2"> <cite> <strong> {{ $t('PRESENTATION.NAME')}} : </strong></cite> T&eacute;o  Berguerre</h2>
-        <h2 class="l3"> <cite> <strong> {{ $t('PRESENTATION.CODENAME')}} : </strong></cite> Crakenar </h2>
-        <h2 class="l4"> <cite> <strong> {{ $t('PRESENTATION.OCCUPATION')}} : </strong></cite> {{ $t('PRESENTATION.OCCUPATION2')}}</h2>
-        <h2 class="l5"> <cite> <strong> {{ $t('PRESENTATION.MAIL')}} : </strong></cite> teo.berguerre&commat;hotmail.com </h2>
-        <h2 class="l6"> <cite> <strong> {{ $t('PRESENTATION.ADDRESS')}} : </strong></cite> 10 place des Jacobins 38130, Echirolles</h2>
-        <h2 class="l7"> <cite> <strong> {{ $t('PRESENTATION.HOBBIES')}} : </strong></cite> {{ $t('PRESENTATION.HOBBIES2')}}</h2>
-        <h2 class="l8"> <cite> <strong> {{ $t('PRESENTATION.CHARACTERISTICS')}} : </strong></cite> {{ $t('PRESENTATION.CHARACTERISTICS2')}}</h2>
+        <h2 class="l2"><cite> <strong> {{ $t('PRESENTATION.NAME') }} : </strong></cite> T&eacute;o Berguerre</h2>
+        <h2 class="l3"><cite> <strong> {{ $t('PRESENTATION.CODENAME') }} : </strong></cite> Crakenar </h2>
+        <h2 class="l4"><cite> <strong> {{ $t('PRESENTATION.OCCUPATION') }} : </strong></cite>
+          {{ $t('PRESENTATION.OCCUPATION2') }}</h2>
+        <h2 class="l5"><cite> <strong> {{ $t('PRESENTATION.MAIL') }} : </strong></cite> teo.berguerre&commat;hotmail.com
+        </h2>
+        <h2 class="l6"><cite> <strong> {{ $t('PRESENTATION.ADDRESS') }} : </strong></cite> 10 place des Jacobins 38130,
+          Echirolles</h2>
+        <h2 class="l7"><cite> <strong> {{ $t('PRESENTATION.HOBBIES') }} : </strong></cite>
+          {{ $t('PRESENTATION.HOBBIES2') }}</h2>
+        <h2 class="l8"><cite> <strong> {{ $t('PRESENTATION.CHARACTERISTICS') }} : </strong></cite>
+          {{ $t('PRESENTATION.CHARACTERISTICS2') }}</h2>
         <div class="row">
         </div>
         <i></i>
@@ -20,23 +25,30 @@
     </div>
   </div>
   <div class="row" style="margin-top: 200px">
-    <div class="col text-center">
-      <p style="text-align: left">Level : {{yearExp}}</p>
-      <div class="col dresseur"> <img :src="URL_DRESSEUR_PIC" alt="icon of an angular image"></div>
-      <div class="progress">
-        <div class="progress-bar" :style="stringFinal"></div>
+    <div class="col">
+      <div class="col dresseur">
+        <img :src="URL_DRESSEUR_PIC" alt="icon of an angular image">
       </div>
-      <div class="row">
-        <div class="col">Connaissances  {{yearExp}}</div>
-        <div class="col">Apprentissage</div>
+      <div class="imgLevel" data-bs-toggle="tooltip" title="how many years of experience since my first internship">
+        <img :src="URL_LEVEL" alt="free icon of a level by Freepik">
+        <span>{{ yearExp }}</span>
+      </div>
+      <div class="progress" style="margin: 10px;">
+        <div :style="stringFinal" class="progress-bar"></div>
+      </div>
+      <div class="code-block2 row">
+        <div class="col-lg-6">Maitrise du Framework ?</div>
+        <div class="col-lg-6">Apprentissage ?</div>
+        <div class="col-lg-6">Interet ?</div>
+        <div class="col-lg-6">Fuite ?</div>
       </div>
     </div>
     <div class="col">
-      <Carousel />
+      <Carousel/>
     </div>
   </div>
   <div class="row" style="text-align: center">
-    <div class="code-block"></div>
+
   </div>
 
 </template>
@@ -47,12 +59,13 @@ import Carousel from './Carousel'
 let month = moment().format('M');
 let percentageMonthWidth = month / 12 * 100;
 let stringFinal = 'width : ' + percentageMonthWidth + '%';
-let yearExp = moment("2020", "YYYY").fromNow().slice(0,2);
+let yearExp = moment("2020", "YYYY").fromNow().slice(0, 2);
 
 const URL_PROFIL_PIC = require('../assets/img/Presentation/1655844360365.jpeg');
 const URL_DRESSEUR_PIC = require('../assets/img/Presentation/dresseur.gif');
+const URL_LEVEL = require('../assets/img/Presentation/icons/level-up.png');
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 @import '../assets/SCSS/presentation.scss';
 </style>
