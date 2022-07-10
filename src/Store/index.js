90a3aa89a39960a037ce163f1dd93d3188eae35e
Teo Berguerre <teothou@hotmail.fr>
2022-07-10 17:22:22 +0200

    printing a bubble message depending of the skill + tech you choosed.... + animation

--- src/Store/index.js
+++ src/Store/index.js
@@ -2,7 +2,12 @@ import {reactive} from "vue";
 
 export const store = reactive({
     TechSelected: "",
+    SkillSelected: 0,
     changeTech(value) {
         this.TechSelected = value;
+    },
+
+    changeSkill(value){
+        this.SkillSelected = value;
     }
 });
