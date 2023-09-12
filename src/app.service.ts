feeb805ce01a3df552866bf739fa3d6f60c8deea
Crakenar <teothou@hotmail.fr>
2023-09-12 22:53:16 +0200

    oupsy

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common/decorators';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World! s';
-  }
-}
--- src/interfaces/user.interace.ts
+++
@@ -1,7 +0,0 @@
-export interface UserInterace {
-    email: string;
-    password: string;
-    name?: string;
-    lastname?: string;
-    age?: number;
-}
